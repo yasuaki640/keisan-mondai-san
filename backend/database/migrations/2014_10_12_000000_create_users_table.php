c3cba2dd0dc315fdd1613dadcf9c0a8267fdb392
yasuaki640 <yasuaki640@gmail.com>
2021-05-16 20:28:23 +0900

    ;lkjouhbih

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->date('d_o_b');
+            $table->tinyInteger('sex');
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
