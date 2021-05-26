4b0e464a63f5f0420223231d161aee3fd1eb5953
yasuaki640 <yasuaki640@gmail.com>
2021-05-26 21:57:15 +0900

    なんの意味もないコミット

---
+++ backend/database/migrations/2021_05_26_125634_create_question_results.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionResults extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_results', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_results');
+    }
+}
