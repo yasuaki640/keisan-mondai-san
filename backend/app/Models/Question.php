5675ee2718392925f28c81bdfa2fb5c73b34a8fd
yasuaki640 <yasuaki640@gmail.com>
2021-05-28 22:44:26 +0900

    うーんやっぱわかんないスネさときんぐ

---
+++ backend/app/Models/Question.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Question extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'sentence',
+        'answer',
+        'has_decimal_point',
+        'operator',
+        'has_minus'
+    ];
+}
