afc2faa6070b29a5d881d07ff154a77368dccd4b
yasuaki640 <yasuaki640@gmail.com>
2021-05-28 22:24:51 +0900

    satoking

--- backend/database/migrations/2021_05_26_124432_create_questions_table.php
+++ backend/database/migrations/2021_05_26_124432_create_questions_table.php
@@ -16,7 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('sentence');
-            $table->string('answer');
+            $table->decimal('answer');
             $table->timestamps();
             $table->softDeletes();
         });
