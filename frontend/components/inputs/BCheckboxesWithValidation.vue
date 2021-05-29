4ad262ce5fc2828659fd8af8ef168b48c2b5c809
yasuaki640 <yasuaki640@gmail.com>
2021-05-29 22:42:14 +0900

    パク・テファン

---
+++ frontend/components/inputs/BCheckboxesWithValidation.vue
@@ -0,0 +1,32 @@
+<template>
+  <ValidationProvider
+    v-slot="{ errors }"
+    :vid="vid"
+    :name="$attrs.label"
+    :rules="rules"
+  >
+    <div class="block">
+      <slot />
+      <p class="has-text-danger">{{ errors[0] }}</p>
+    </div>
+  </ValidationProvider>
+</template>
+
+<script>
+import { ValidationProvider } from 'vee-validate'
+
+export default {
+  components: {
+    ValidationProvider,
+  },
+  props: {
+    vid: {
+      type: String,
+    },
+    rules: {
+      type: [Object, String],
+      default: '',
+    },
+  },
+}
+</script>
---
+++ frontend/components/inputs/BSelectWithValidation.vue
@@ -0,0 +1,59 @@
+<template>
+  <ValidationProvider
+    v-slot="{ errors, valid }"
+    :vid="vid"
+    :name="$attrs.label"
+    :rules="rules"
+  >
+    <b-field
+      v-bind="$attrs"
+      :type="{ 'is-danger': errors[0], 'is-success': valid }"
+      :message="errors"
+    >
+      <b-select v-model="innerValue" placeholder="Select a subject">
+        <slot />
+      </b-select>
+    </b-field>
+  </ValidationProvider>
+</template>
+
+<script>
+import { ValidationProvider } from 'vee-validate'
+
+export default {
+  components: {
+    ValidationProvider,
+  },
+  props: {
+    vid: {
+      type: String,
+    },
+    rules: {
+      type: [Object, String],
+      default: '',
+    },
+    // must be included in props
+    value: {
+      type: null,
+    },
+  },
+  data: () => ({
+    innerValue: '',
+  }),
+  watch: {
+    // Handles internal model changes.
+    innerValue(newVal) {
+      this.$emit('input', newVal)
+    },
+    // Handles external model changes.
+    value(newVal) {
+      this.innerValue = newVal
+    },
+  },
+  created() {
+    if (this.value) {
+      this.innerValue = this.value
+    }
+  },
+}
+</script>
