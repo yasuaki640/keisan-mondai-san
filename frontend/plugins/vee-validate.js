374d5a726517b002f0cbbb8564429fccfd79e767
yasuaki640 <yasuaki640@gmail.com>
2021-05-31 22:42:23 +0900

    w+iovuh

--- frontend/plugins/vee-validate.js
+++ frontend/plugins/vee-validate.js
@@ -6,12 +6,14 @@ import {
   extend,
 } from 'vee-validate' // 使用する機能
 import ja from 'vee-validate/dist/locale/ja.json' // エラーメッセージの日本語化用
-import { required, max, email, min } from 'vee-validate/dist/rules' // 使用するバリデーションルール
+import { required, max, email, min, confirmed } from 'vee-validate/dist/rules' // 使用するバリデーションルール
 
 // VeeValidateがデフォルトで用意している各ルールを使用するよう指定
-extend('required', required) / extend('email', email)
+extend('required', required)
+extend('email', email)
 extend('max', max)
 extend('min', min)
+extend('confirmed', confirmed)
 
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.component('ValidationObserver', ValidationObserver)
