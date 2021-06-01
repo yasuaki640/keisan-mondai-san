d4c1da2a78891cc3a50cfb27c707659bca7e9240
yasuaki640 <yasuaki640@gmail.com>
2021-06-01 21:38:36 +0900

    drg

---
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(int $id)
+    {
+        //
+    }
+
+    public function edit(int $id)
+    {
+        //
+    }
+
+    public function update(Request $request, int $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ backend/app/Http/Controllers/User/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\CreateRequest;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+
+    public function create(CreateRequest $request)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(int $id)
+    {
+        //
+    }
+
+    public function edit(int $id)
+    {
+        //
+    }
+
+    public function update(Request $request, int $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
