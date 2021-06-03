f56d59136e2334bd39bdc78b28762bbefc1f2f7c
yasuaki640 <yasuaki640@gmail.com>
2021-06-03 22:38:01 +0900

    もう何も怖くない

--- backend/app/Http/Controllers/User/UserController.php
+++ backend/app/Http/Controllers/User/UserController.php
@@ -1,13 +1,22 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\CreateRequest;
+use App\Service\UserSerivce;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    private $service;
+
+    public function __construct(UserSerivce $service)
+    {
+        $this->service = $service;
+    }
+
     public function index()
     {
         //
