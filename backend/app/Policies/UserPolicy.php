b60265d4702336bc921fd64421dffb45d48b375f
yasuaki640 <yasuaki640@gmail.com>
2021-06-10 22:58:12 +0900

    ;adl

---
+++ backend/app/Policies/UserPolicy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(int $id): bool
+    {
+        return $id === auth()->id();
+    }
+}
