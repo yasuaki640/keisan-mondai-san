5f2af7aa351b6256c591d271920da0767c374c1f
yasuaki640 <yasuaki640@gmail.com>
2021-06-15 22:29:06 +0900

    09iAGU@

--- backend/tests/Unit/App/Http/Auth/LoginRequestTest.php
+++ backend/tests/Unit/App/Http/Auth/LoginRequestTest.php
@@ -2,19 +2,49 @@
 
 namespace Tests\Unit\App\Http\Auth;
 
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
 class LoginRequestTest extends TestCase
 {
-    public function test_validation($expected, $data)
+    /**
+     * @param array
+     * @param array
+     * @param boolean
+     * @dataProvider dataUserRegistration
+     */
+    public function test_LoginRequest_validation(array $keys, array $values, bool $expect)
     {
-        $this->assertEquals($expected, $data);
+        $dataList = array_combine($keys, $values);
+
+        $rules = (new LoginRequest())->rules();
+
+        $validator = Validator::make($dataList, $rules);
+
+        $this->assertEquals($expect, $validator->passes());
     }
 
-    protected function validate($data)
+    public function dataUserRegistration()
     {
-        
+        return [
+            'no name' => [
+                ['password'],
+                ['pass1234'],
+                false
+            ],
+            'no password' => [
+                ['name'],
+                ['test'],
+                false
+            ],
+            'success' => [
+                ['name', 'password'],
+                ['yasu', 'pass1234'],
+                true
+            ]
+        ];
     }
 }
