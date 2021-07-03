b0a58024c45316b1c029bde0dbb7eedabae6ddf4
yasuaki640 <yasuaki640@gmail.com>
2021-07-03 15:50:12 +0900

    あ

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -39,7 +39,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: 'http://localhost:80/api',
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
