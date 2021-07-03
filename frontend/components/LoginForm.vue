db961a8cddbb289128e46d6f28d12fcceeb90c3f
yasuaki640 <yasuaki640@gmail.com>
2021-07-03 19:54:56 +0900

    loginのテンプレ用意

---
+++ frontend/components/LoginForm.vue
@@ -0,0 +1,74 @@
+<template>
+  <ValidationObserver ref="observer" v-slot="{ handleSubmit }">
+    <!-- the "handleSubmit" function on the slot-scope executes the callback if validation was successfull -->
+    <section class="section">
+      <BInputWithValidation
+        v-model="item.name"
+        rules="required"
+        type="text"
+        label="ユーザーID *"
+      />
+
+      <BInputWithValidation
+        v-model="item.password"
+        rules="required"
+        type="password"
+        label="パスワード *"
+        vid="password"
+      />
+
+      <div class="buttons">
+        <button class="button is-success" @click="handleSubmit(submit)">
+          <span class="icon is-small">
+            <i class="fas fa-check"></i>
+          </span>
+          <span>Submit</span>
+        </button>
+
+        <button class="button" @click="resetForm">
+          <span class="icon is-small">
+            <i class="fas fa-redo"></i>
+          </span>
+          <span>Reset</span>
+        </button>
+      </div>
+    </section>
+  </ValidationObserver>
+</template>
+
+<script>
+import BInputWithValidation from './inputs/BInputWithValidation'
+
+export default {
+  name: 'BuefyForm',
+  components: {
+    BInputWithValidation,
+  },
+  data() {
+    return {
+      item: {
+        name: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    async submit() {
+      await this.$axios.$post('/login', {
+        name: this.item.name,
+        password: this.item.password,
+      })
+      alert('ログインに成功しました。')
+
+      await this.$router.push('/')
+    },
+    resetForm() {
+      this.name = ''
+      this.password = ''
+      requestAnimationFrame(() => {
+        this.$refs.observer.reset()
+      })
+    },
+  },
+}
+</script>
--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -4,17 +4,18 @@
     <h4 class="subtitle is-6 has-text-grey">
       このアプリは小学生から中学生が対象の、四則演算を練習できるアプリです。
     </h4>
-    <nuxt-link to="/register">
-      <b-button type="is-primary">新規登録</b-button>
-    </nuxt-link>
-    <nuxt-link to="/login">
-      <b-button type="is-primary is-light">ログイン</b-button>
-    </nuxt-link>
+    <p class="subtitle is-7">* は入力必須です。</p>
+    <LoginForm></LoginForm>
   </section>
 </template>
 
 <script>
+import LoginForm from '~/components/LoginForm'
+
 export default {
-  name: 'Index',
+  name: 'Login',
+  comments: {
+    LoginForm,
+  },
 }
 </script>
