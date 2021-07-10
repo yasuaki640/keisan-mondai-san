0faa14fd2f7877aed9514d96824868d926b6cde9
yasuaki640 <yasuaki640@gmail.com>
2021-07-10 18:39:01 +0900

    recaofpviad

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -12,6 +12,12 @@
 </template>
 
 <script>
+import {
+  HTTP_STATUS_FORBIDDEN,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_UNAUTHORIZED,
+} from '~/constants/http'
+
 export default {
   layout: 'error',
   props: {
@@ -20,16 +26,40 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      message: '',
+    }
+  },
+  computed: {},
   async created() {
-    if (this.status === 401) {
+    if (
+      this.error.status === HTTP_STATUS_UNAUTHORIZED ||
+      this.error.status === HTTP_STATUS_FORBIDDEN
+    ) {
       await this.$auth.logout
     }
+  },
+  mounted() {
+    this.getMessageByStatusCode(this.error.statusCode)
   },
   methods: {
     redirect() {
       return this.$route.name === 'index'
-        ? this.$router.go()
+        ? this.$router.go(0)
         : this.$router.push('/')
+    },
+    getMessageByStatusCode(statusCode) {
+      if (statusCode === HTTP_STATUS_NOT_FOUND) {
+        return 'ページが存在しません。'
+      } else if (
+        statusCode === HTTP_STATUS_UNAUTHORIZED ||
+        statusCode === HTTP_STATUS_FORBIDDEN
+      ) {
+        return 'ログインし直してください。'
+      } else {
+        return '予期せぬエラーが発生しました。大変申し訳ありませんがもう一度操作をお試しください。'
+      }
     },
   },
 }
