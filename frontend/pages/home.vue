c0d7ff5964e6cd53caa5be50cba8dc3bd604a697
yasuaki640 <yasuaki640@gmail.com>
2021-07-10 19:01:56 +0900

    やっつけ仕事出面語

---
+++ frontend/pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 class="title">ユーザーホーム画面</h1>
+    <Card title="問題を解く" icon="cellphone-link"></Card>
+    <Card title="成績確認" icon="alert-decagram"></Card>
+    <Card title="プロフィール編集" icon="arrange-bring-to-front"></Card>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
