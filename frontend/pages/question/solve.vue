1c86757b94681239ebaa37c88f8df28e0d9bc560
yasuaki640 <yasuaki640@gmail.com>
2021-07-11 20:54:43 +0900

    l;k;:k

--- frontend/pages/question/solve.vue
+++ frontend/pages/question/solve.vue
@@ -1,10 +1,19 @@
 <template>
   <section class="section">
     <h2 class="title is-3 has-text-grey">解答入力画面</h2>
+    <BSelectWithValidation v-model="subject" rules="required" label="演算子">
+      <option value>選択してください</option>
+      <option value="add">足し算</option>
+    </BSelectWithValidation>
   </section>
 </template>
 <script>
+import BSelectWithValidation from '~/components/inputs/BSelectWithValidation'
+
 export default {
+  components: {
+    BSelectWithValidation,
+  },
   middleware: 'auth',
 }
 </script>
