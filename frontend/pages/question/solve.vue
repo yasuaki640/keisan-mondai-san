bf028a4f92f5dd337f8544a530e9392ddcabc54d
yasuaki640 <yasuaki640@gmail.com>
2021-07-11 21:07:49 +0900

    まあとりあえず今日はここまであえあえげｒ

--- frontend/pages/question/solve.vue
+++ frontend/pages/question/solve.vue
@@ -1,18 +1,37 @@
 <template>
   <section class="section">
-    <h2 class="title is-3 has-text-grey">解答入力画面</h2>
-    <BSelectWithValidation v-model="subject" rules="required" label="演算子">
+    <h2 class="title is-3 has-text-grey">問題種別設定画面</h2>
+
+    <BSelectWithValidation rules="required" label="演算子">
       <option value>選択してください</option>
       <option value="add">足し算</option>
     </BSelectWithValidation>
+
+    <BRadioWithValidation
+      rules="required"
+      name="num_of_questions"
+      type="radio"
+      label="問題数"
+    >
+      <b-radio native-value="10">10問</b-radio>
+      <b-radio native-value="20">20問</b-radio>
+      <b-radio native-value="30">30問</b-radio>
+      <b-radio native-value="50">50問</b-radio>
+    </BRadioWithValidation>
+
+    <nuxt-link to="/login">
+      <b-button type="is-primary">回答開始</b-button>
+    </nuxt-link>
   </section>
 </template>
 <script>
 import BSelectWithValidation from '~/components/inputs/BSelectWithValidation'
+import BRadioWithValidation from '~/components/inputs/BRadioWithValidation'
 
 export default {
   components: {
     BSelectWithValidation,
+    BRadioWithValidation,
   },
   middleware: 'auth',
 }
