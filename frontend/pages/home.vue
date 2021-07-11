7e0e032ef6e9e5b439b3f6109d27c805ba782ea9
yasuaki640 <yasuaki640@gmail.com>
2021-07-11 19:34:35 +0900

    私は天才なのでリファクタリングも天才的に行う

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -1,19 +1,36 @@
 <template>
   <section>
     <h1 class="title">ユーザーホーム画面</h1>
-    <Card
-      title="問題を解く"
-      icon="cellphone-link"
-      link="/question/select-questions"
-    >
-    </Card>
-    <Card title="成績確認" icon="alert-decagram"></Card>
-    <Card title="プロフィール編集" icon="arrange-bring-to-front"></Card>
+    <template v-for="(item, key) of items">
+      <Card :key="key" :title="item.title" :icon="item.icon" :to="item.to">
+      </Card>
+    </template>
   </section>
 </template>
 
 <script>
 export default {
   middleware: 'auth',
+  data() {
+    return {
+      items: [
+        {
+          title: '問題を解く',
+          icon: 'cellphone-link',
+          to: { name: 'question-select' },
+        },
+        {
+          title: '成績確認',
+          icon: 'alert-decagram',
+          to: { name: 'question-select' },
+        },
+        {
+          title: 'プロフィール編集',
+          icon: 'arrange-bring-to-front',
+          to: { name: 'question-select' },
+        },
+      ],
+    }
+  },
 }
 </script>
