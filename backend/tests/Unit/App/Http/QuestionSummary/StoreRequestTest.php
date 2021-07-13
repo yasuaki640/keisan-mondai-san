88337dbe8247edc955213201f7a22dfa018adbe9
yasuaki640 <yasuaki640@gmail.com>
2021-07-13 22:44:47 +0900

    i:jlkk

---
+++ backend/tests/Unit/App/Http/QuestionSummary/StoreRequestTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Unit\App\Http\QuestionSummary;
+
+use App\Http\Requests\QuestionSummary\StoreRequest;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class StoreRequestTest extends TestCase
+{
+    /**
+     * @param array
+     * @param array
+     * @param boolean
+     * @dataProvider dataProvider
+     */
+    public function test_StoreRequest_validation(array $keys, array $values, bool $expect)
+    {
+        $dataList = array_combine($keys, $values);
+
+        $rules = (new StoreRequest())->rules();
+
+        $validator = Validator::make($dataList, $rules);
+
+        $this->assertEquals($expect, $validator->passes());
+    }
+
+    public function dataProvider()
+    {
+        return [
+            'fail invalid operator' => [
+                [
+                    'operator' ,
+                    'num_of_questions'
+
+                ],
+                [
+                    'aaa',
+                    123
+                ],
+                false
+            ],
+            'success operator is add' => [
+                [
+                    'operator' ,
+                    'num_of_questions'
+
+                ],
+                [
+                    'add',
+                    123
+                ],
+                true
+            ],
+            'success operator is sub' => [
+                [
+                    'operator' ,
+                    'num_of_questions'
+
+                ],
+                [
+                    'sub',
+                    123
+                ],
+                true
+            ],
+            'success operator is multi' => [
+                [
+                    'operator' ,
+                    'num_of_questions'
+
+                ],
+                [
+                    'multi',
+                    123
+                ],
+                true
+            ],
+            'success operator is divide' => [
+                [
+                    'operator' ,
+                    'num_of_questions'
+
+                ],
+                [
+                    'divide',
+                    123
+                ],
+                true
+            ],
+        ];
+    }
+}
