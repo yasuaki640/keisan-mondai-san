28ac3ed6435de14be86fea1d9cf5859c08447763
yasuaki640 <yasuaki640@gmail.com>
2021-07-17 22:19:30 +0900

    a]iigjwpr

--- backend/app/Models/QuestionSummary.php
+++ backend/app/Models/QuestionSummary.php
@@ -10,6 +10,21 @@ class QuestionSummary extends Model
 {
     use HasFactory, SoftDeletes;
 
+    public const OPERATOR_ADD = 'add';
+
+    public const OPERATOR_SUB = 'sub';
+
+    public const OPERATOR_MULTI = 'multi';
+
+    public const OPERATOR_DIVIDE = 'divide';
+
+    public const OPERATOR_LIST = [
+        self::OPERATOR_ADD,
+        self::OPERATOR_SUB,
+        self::OPERATOR_MULTI,
+        self::OPERATOR_DIVIDE
+    ];
+
     protected $fillable = [
         'user_id',
         'num_of_questions',
