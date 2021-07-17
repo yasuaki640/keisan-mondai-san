37e8aa76a4cc3d8e8724eefebc95aea526f6869f
yasuaki640 <yasuaki640@gmail.com>
2021-07-17 22:18:01 +0900

    ふぁけｒ

---
+++ backend/database/factories/QuestionSummaryFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\QuestionSummary;
+use App\Models\QuestonSummary;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionSummaryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = QuestionSummary::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        $answerStartAt = $this->faker->dateTimeBetween('-1year', '-1day');
+
+        return [
+            'user_id' => User::factory()->create()->id,
+            'num_of_questions' => $this->faker->numberBetween(1, 100),
+            'operator' => $this->getRandomOperator(),
+            'answer_start_at' => $answerStartAt,
+            'answer_end_at' => $answerStartAt->modify('+10minutes'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+
+    private function getRandomOperator(): string
+    {
+        $key = array_rand(QuestionSummary::OPERATOR_LIST);
+        return QuestionSummary::OPERATOR_LIST[$key];
+    }
+}
