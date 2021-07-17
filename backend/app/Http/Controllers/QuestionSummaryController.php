904f45cfe21212b63d4cc926d9e2c7a8843cea09
yasuaki640 <yasuaki640@gmail.com>
2021-07-17 22:54:34 +0900

    O!{#TEGIUPVW

--- backend/app/Http/Controllers/QuestionSummaryController.php
+++ backend/app/Http/Controllers/QuestionSummaryController.php
@@ -10,15 +10,30 @@ use App\Service\QuestionSummary\QuestionSummaryServiceImpl;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
+/**
+ * Class QuestionSummaryController
+ * @package App\Http\Controllers
+ */
 class QuestionSummaryController extends Controller
 {
+    /**
+     * @var QuestionSummaryService
+     */
     private QuestionSummaryService $service;
 
+    /**
+     * QuestionSummaryController constructor.
+     * @param QuestionSummaryServiceImpl $service
+     */
     public function __construct(QuestionSummaryServiceImpl $service)
     {
         $this->service = $service;
     }
 
+    /**
+     * @param StoreRequest $request
+     * @return JsonResponse
+     */
     public function store(StoreRequest $request): JsonResponse
     {
         $id = $this->service->store(array_merge($request->validated()));
@@ -26,6 +41,10 @@ class QuestionSummaryController extends Controller
         return response()->json(['id' => $id], Response::HTTP_CREATED);
     }
 
+    /**
+     * @param QuestionSummary $questionSummary
+     * @return JsonResponse
+     */
     public function show(QuestionSummary $questionSummary)
     {
         return response()->json(new ShowResource($questionSummary));
--- backend/app/Http/Controllers/User/UserController.php
+++ backend/app/Http/Controllers/User/UserController.php
@@ -17,37 +17,68 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
+/**
+ * Class UserController
+ * @package App\Http\Controllers\User
+ */
 class UserController extends Controller
 {
+    /**
+     * @var UserService
+     */
     private UserService $service;
 
+    /**
+     * UserController constructor.
+     * @param UserServiceImpl $service
+     */
     public function __construct(UserServiceImpl $service)
     {
         $this->service = $service;
     }
 
+    /**
+     * @return JsonResponse
+     */
     public function index(): JsonResponse
     {
         $users = $this->service->index();
         return response()->json(IndexResource::collection($users));
     }
 
+    /**
+     * @return JsonResponse
+     */
     public function me(): JsonResponse
     {
         return response()->json(new ShowResource(auth()->user()));
     }
 
+    /**
+     * @param StoreRequest $request
+     * @return JsonResponse
+     */
     public function store(StoreRequest $request): JsonResponse
     {
         $id = $this->service->store($request->all());
         return response()->json(new StoreResource($id), Response::HTTP_CREATED);
     }
 
+    /**
+     * @param User $user
+     * @return JsonResponse
+     */
     public function show(User $user): JsonResponse
     {
         return response()->json(new IndexResource($user));
     }
 
+    /**
+     * @param UpdateRequest $request
+     * @param User $user
+     * @return JsonResponse
+     * @throws AuthenticationException
+     */
     public function update(UpdateRequest $request, User $user): JsonResponse
     {
         if ($user->id !== auth()->id()) throw new AuthenticationException("Can't update other than the logged-in user");
@@ -56,6 +87,11 @@ class UserController extends Controller
         return response()->json(new UpdateResource($user));
     }
 
+    /**
+     * @param User $user
+     * @return JsonResponse
+     * @throws AuthenticationException
+     */
     public function destroy(User $user): JsonResponse
     {
         if ($user->id !== auth()->id()) throw new AuthenticationException("Can't delete other than the logged-in user");
