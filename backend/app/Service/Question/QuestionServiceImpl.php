6c2a46a663fd9bcf332dd0a272f9382870a3e56f
yasuaki640 <yasuaki640@gmail.com>
2021-07-22 23:09:05 +0900

    wij:berk :;

--- backend/app/Service/Question/QuestionServiceImpl.php
+++ backend/app/Service/Question/QuestionServiceImpl.php
@@ -6,9 +6,30 @@ namespace App\Service\Question;
 
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * Class QuestionServiceImpl
+ * @package App\Service\Question
+ */
 class QuestionServiceImpl implements QuestionService
 {
+    /**
+     * @var QuestionRepository
+     */
+    private QuestionRepository $repository;
 
+
+    /**
+     * QuestionServiceImpl constructor.
+     */
+    public function __construct(QuestionRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param int $questionSummaryId
+     * @return array|Collection
+     */
     public function storeByQuestionSummaryId(int $questionSummaryId): array|Collection
     {
         return [];
