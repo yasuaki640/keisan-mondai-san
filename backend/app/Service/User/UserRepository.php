5cbc81ffdc87d6d0db5d609a011f39ca9696a6dc
yasuaki640 <yasuaki640@gmail.com>
2021-07-23 20:42:40 +0900

    @oewi

--- backend/app/Service/User/UserRepository.php
+++ backend/app/Service/User/UserRepository.php
@@ -7,18 +7,34 @@ namespace App\Service\User;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * Class UserRepository
+ * @package App\Service\User
+ */
 class UserRepository
 {
+    /**
+     * @param array $params
+     * @return int
+     */
     public function create(array $params): int
     {
         return User::create($params)->id;
     }
 
+    /**
+     * @return Collection|array
+     */
     public function findAll(): Collection|array
     {
         return User::all();
     }
 
+    /**
+     * @param int $id
+     * @param array $params
+     * @return User
+     */
     public function update(int $id, array $params): User
     {
         $user = User::find($id);
@@ -27,6 +43,9 @@ class UserRepository
         return $user;
     }
 
+    /**
+     * @param int $id
+     */
     public function destroy(int $id)
     {
         User::find($id)->delete();
--- backend/app/Service/User/UserService.php
+++ backend/app/Service/User/UserService.php
@@ -7,13 +7,32 @@ namespace App\Service\User;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * Interface UserService
+ * @package App\Service\User
+ */
 interface UserService
 {
+    /**
+     * @param array $params
+     * @return int
+     */
     public function store(array $params): int;
 
+    /**
+     * @return Collection|array
+     */
     public function index(): Collection|array;
 
+    /**
+     * @param int $id
+     * @param array $params
+     * @return User
+     */
     public function update(int $id, array $params): User;
 
+    /**
+     * @param int $id
+     */
     public function destroy(int $id): void;
 }
